c348e5990d57132ac33abd40d16caf3286bac298
Fillipe Diord <fillipe.diord@hotmail.com>
2024-02-07 15:37:01 -0300

    feat: 🎸 create checkbox component
    create checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,11 @@
+import Checkbox from 'expo-checkbox';
+
+import { styles } from './styles';
+
+export function CheckBox() {
+  return (
+    <>
+      <Checkbox style={styles.checkbox} />
+    </>
+  )
+} 
